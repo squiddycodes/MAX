fed4277c3d73925c18e8f5affb85f38743121bcd
squiddycodes <dguidorizzi@yahoo.com>
2025-10-25 14:07:52 -0400

    Adding project files

--- MAX/asgi.py
+++ MAX/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Max project.
+ASGI config for MAX project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Max.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAX.settings')
 
 application = get_asgi_application()
--- MAX/wsgi.py
+++ MAX/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Max project.
+WSGI config for MAX project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Max.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAX.settings')
 
 application = get_wsgi_application()
